e0f6e8cb0ef92acbe175e3e90bf60ee46e83bf09
Sharan Thakur <sharandt19@live.com>
2024-03-09 11:26:22 +0530

    Initial Commit

---
+++ PetsImageClassifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetsImageClassifier
+//
+//  Created by Sharan Thakur on 09/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetsImageClassifier/PetsImageClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsImageClassifierApp.swift
+//  PetsImageClassifier
+//
+//  Created by Sharan Thakur on 09/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PetsImageClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
